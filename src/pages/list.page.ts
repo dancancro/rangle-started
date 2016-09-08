6f3011512fadf8b3c86d00bce2b8af9079ec859b
dancancro <dan.cancro@gmail.com>
2016-09-08 19:57:13 -0400

    blah

--- src/pages/list.page.ts
+++ src/pages/list.page.ts
@@ -13,6 +13,9 @@ import { NgRedux } from 'ng2-redux';
 import { select } from 'ng2-redux';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/zip';
+
 import { DataService } from '../services/data.service';
 import { IObjection } from '../store';
 import { IList } from '../store/list/list.types';
@@ -38,15 +41,45 @@ export class ListPage implements OnInit {
   };
 
   constructor(
+    private route: ActivatedRoute,
     public listActions: ListActions,
     private dataService: DataService) {
   }
 
   ngOnInit() {
     // TODO: Maybe this should happen in a new ngModule
-    this.subscription = this.dataService.getObjections().subscribe({
-        next: (objections) => this.listActions.fetchObjections(objections),
-        error: (err) => this.listActions.error(err)
-      });
+    // this.subscription = this.dataService.getObjections().subscribe({
+    //     next: (objections) => this.listActions.fetchObjections(objections),
+    //     error: (err) => this.listActions.error(err)
+    //   });
+
+
+    Observable.forkJoin(
+      this.route.params.map(params => params['objection']),
+      this.dataService.getObjections().map(objections => objections)).subscribe(
+        (res: Array<any>) => {
+//          debugger;
+          let objections = res[1];
+          this.listActions.fetchObjections(objections);
+          let objectionId = res[0];
+          if (objectionId) {
+            this.listActions.fetchObjections(res[1]);
+            let objection = objections.find(o => o.id === +objectionId);
+            let y = document.getElementById(objectionId).getBoundingClientRect().top - 100;
+            window.scrollBy(0, y);
+            this.listActions.toggleRebuttals({objection: objection});
+          }
+        },
+        (err) => {
+//          debugger;
+          this.listActions.error(err);
+        },
+        () => {
+//          debugger;
+          console.log('done');
+        }
+      );
+
+
   }
 }
